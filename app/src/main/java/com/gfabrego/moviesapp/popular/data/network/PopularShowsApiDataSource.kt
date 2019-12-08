3c7418f9ac92da7919c5a405447b3451cfaa9f77
abeade <albert.beade@letgo.com>
2019-12-08 18:26:00 +0100

    Flow creation alternative

--- app/src/main/java/com/gfabrego/moviesapp/popular/data/network/PopularShowsApiDataSource.kt
+++ app/src/main/java/com/gfabrego/moviesapp/popular/data/network/PopularShowsApiDataSource.kt
@@ -3,10 +3,8 @@ package com.gfabrego.moviesapp.popular.data.network
 import com.gfabrego.moviesapp.BuildConfig
 import com.gfabrego.moviesapp.popular.domain.model.PageRequest
 import com.gfabrego.moviesapp.popular.domain.model.PopularShowsResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 internal class PopularShowsApiDataSource(
@@ -17,4 +15,12 @@ internal class PopularShowsApiDataSource(
     fun getPopularShows(pageRequest: PageRequest.Paged): Flow<PopularShowsResponse> =
         flow { emit(service.getPopularShows(BuildConfig.API_KEY, pageRequest.page)) }
             .map(apiShowsMapper::map)
+
+//    fun getPopularShows(pageRequest: PageRequest.Paged): Flow<PopularShowsResponse> {
+//
+//        suspend fun remoteCall(): PopularShowsApiResponse =
+//            service.getPopularShows(BuildConfig.API_KEY, pageRequest.page)
+//
+//        return ::remoteCall.asFlow().map(apiShowsMapper::map)
+//    }
 }
