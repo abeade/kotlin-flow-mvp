e752fe4fe7a5443c7faf3312ede972ddd8065aec
abeade <albert.beade@letgo.com>
2019-12-08 12:13:42 +0100

    Test delay and coroutine context cancellation

--- app/src/main/java/com/gfabrego/moviesapp/popular/data/network/PopularShowsApiDataSource.kt
+++ app/src/main/java/com/gfabrego/moviesapp/popular/data/network/PopularShowsApiDataSource.kt
@@ -3,8 +3,10 @@ package com.gfabrego.moviesapp.popular.data.network
 import com.gfabrego.moviesapp.BuildConfig
 import com.gfabrego.moviesapp.popular.domain.model.PageRequest
 import com.gfabrego.moviesapp.popular.domain.model.PopularShowsResponse
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.transform
 
 internal class PopularShowsApiDataSource(
     private val service: PopularShowsService,
@@ -12,5 +14,10 @@ internal class PopularShowsApiDataSource(
 ) {
 
     fun getPopularShows(pageRequest: PageRequest.Paged): Flow<PopularShowsResponse> =
-        service.getPopularShows(BuildConfig.API_KEY, pageRequest.page).map(apiShowsMapper::map)
+        service.getPopularShows(BuildConfig.API_KEY, pageRequest.page)
+            .transform { value ->
+                delay(5000)
+                return@transform emit(value)
+            }
+            .map(apiShowsMapper::map)
 }
